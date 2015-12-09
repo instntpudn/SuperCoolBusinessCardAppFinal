c3e1b5928a79cd4db110285b8b65e71ff4f8a32c
agiorgio <agiorgio@student.sjcny.edu>
2015-12-09 18:13:27 -0500

    test commit

---
+++ app/src/main/java/sjcnymobile/supercoolbusinesscardapp/SplashScreen.java
@@ -0,0 +1,40 @@
+package supercoolbusinessapp.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import sjcnymobile.view.R;
+
+/**
+ * Created by Anthony on 11/15/2015.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashpage);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
