e5d7f92064ede723b08bfaed08381b22981bb9c3
aalvarez2 <akas82182@gmail.com>
2015-12-14 17:08:41 -0500

    Added contacts class and layout

--- app/src/main/java/sjcnymobile/supercoolbusinesscardapp/MainActivity.java
+++ app/src/main/java/sjcnymobile/supercoolbusinesscardapp/MainActivity.java
@@ -1,7 +1,8 @@
 package sjcnymobile.supercoolbusinesscardapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, Contacts.class);
+        startActivity(intent);
+
     }
 }
