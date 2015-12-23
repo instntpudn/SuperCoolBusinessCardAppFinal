b4fc0f2a03796707ba97c7ef0107a42fb3e763f6
Anthony <atk2@hotmail.com>
2015-12-23 13:05:00 -0500

    GetCard

---
+++ app/src/main/java/sjcnymobile/supercoolbusinesscardapp/managers/ListItem.java
@@ -0,0 +1,37 @@
+package sjcnymobile.supercoolbusinesscardapp.managers;
+
+
+/**
+ * Created by Anthony on 12/14/2015.
+ */
+
+public class ListItem {
+
+    private String company;
+    private String compid;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String headline) {
+        this.company = headline;
+    }
+
+    public String getCompId() {
+        return compid;
+    }
+
+    public void setCompId(String compid) {
+        this.compid = compid;
+    }
+}
